97a82e52d847afe82b665e455e3f866165291699
Mateus Alencar <mtauws@gmail.com>
2020-07-26 16:36:01 -0300

    model / post and controller / post added, multer configuration files and route files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload')
 
 const routes = new express.Router();
+const upload = multer(uploadConfig);
 
+const PostController = require('./controllers/PostController');
 
-routes.get('/', (req, res) => {
-  res.send('coe');
-});
-
+routes.get('/posts', PostController.index);  
+routes.post('/posts', upload.single('image'), PostController.store);  
 
 module.exports = routes;
